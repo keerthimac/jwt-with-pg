7a99bb1f7b1ab282af9c9ec6a70fa5036be32d73
Darshana <keerthi.mac@gmail.com>
2022-01-20 00:14:54 +0530

    register and login routets complected

--- routes/jwtAuth.js
+++ routes/jwtAuth.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 const pool = require("../db");
 const bcrypt = require("bcryptjs");
+const jwtGenerator = require("../utils/jwtGenerator");
 
-
+//Register Route
 router.post("/register", async (req, res) => {
   try {
     //01. destructure the req.body (name, email, password)
     const { name, email, password } = req.body;
-    console.log(req.body);
-    console.log(password);
+    //console.log(req.body);
+    //console.log(password);
 
     //02. check if the user already exists in the database (if so throw error)
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
@@ -26,14 +27,52 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
     //console.log(hashedPassword);
-    
+
     //04. insert the user into the database
     const newUser = await pool.query(
       "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
       [name, email, hashedPassword]
     ); //returns the new user object
-    res.json(newUser.rows[0]);
-    //05. generate a  jwt token
+    console.log(newUser.rows[0].user_id);
+
+    //05. generate a  jwt token and send it back to the client
+    const token = jwtGenerator(newUser.rows[0].user_id); //from utils/jwtGenerator.js
+    res.json({ token });
+
+    //------------------------------------------------------------------
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//Login Route
+router.post("/login", async (req, res) => {
+  try {
+    //01 Destucture the req.body (email, password)
+    const { email, password } = req.body;
+
+    //02. check if the user exists in the database
+    const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [
+      email,
+    ]);
+    if (user.rows.length === 0) {
+      return res.status(400).json({
+        status: 400,
+        error: "User does not exist",
+      });
+    }
+    //03. check if the password is correct
+    const isMatch = await bcrypt.compare(password, user.rows[0].user_password);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: 400,
+        error: "Password is incorrect",
+      });
+    }
+    //04. generate a jwt token and send it back to the client
+    const token = jwtGenerator(user.rows[0].user_id); //from utils/jwtGenerator.js
+    res.json({ token });
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
---
+++ utils/jwtGenerator.js
@@ -0,0 +1,12 @@
+//jwt generator
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwtGenerator = (user_id) => {
+  const jwtSecret = process.env.JWT_SECRET;
+  const payload = { user_id };
+  const token = jwt.sign(payload, jwtSecret, { expiresIn: "1h" });
+  return token;
+};
+
+module.exports = jwtGenerator;
