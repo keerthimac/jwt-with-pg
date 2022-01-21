f6f4a0a5bd0f9dbdc80dcc7201c26321fc3f7227
keeth-office <keerthi.mac@gmail.com>
2022-01-21 19:01:01 +0530

    login & register componets

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,16 +1,37 @@
 import {Link} from 'react-router-dom';
+import {useState} from 'react';
 
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      email,
+      password
+    }
+    console.log(user);
+  };
   return (
     <div>
-      <h1>Login</h1>
-      <Link to="/dashboard">
+      <h1 className='text-center'>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input className='form-control my-3' value={email} type="email" placeholder="email" onChange={(e)=>setEmail(e.target.value)} />
+        <input className='form-control my-3' value={password} type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+        <button className='btn btn-success block' type="submit">Login</button>
+      </form>
+
+
+
+      {/* <Link to="/dashboard">
         <button className='btn btn-primary m-2'>Dashboard</button>
       </Link>
       <Link to="/">
         <button className='btn btn-primary m-2'>Register</button>
-      </Link>
+      </Link> */}
 
     </div>
   );
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,15 +1,39 @@
 import {Link} from 'react-router-dom';
+import{useState} from 'react';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //const [error, setError] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+      name,
+      email,
+      password
+
+    }
+    console.log(user);
+  };
   return (
     <div>
-      <h1>Register</h1>
-      <Link to="/login">
+      <h1 className='text-center'>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input required className='form-control my-3' value={name} type="name" placeholder="name" onChange={(e)=>setName(e.target.value)} />
+        <input required className='form-control my-3' value={email} type="email" placeholder="email" onChange={(e)=>setEmail(e.target.value)} />
+        <input required className='form-control my-3' value={password} type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
+        <div className="d-grid gap-2">
+        <button className='btn btn-success' type="submit">Login</button>
+        </div>
+      </form>
+
+      {/* <Link to="/login">
         <button className='btn btn-primary m-2'>Login</button>
-      </Link>
-      {/* <Link to="/dashboard">
-        <button className='btn btn-primary m-2'>Dashboard</button>
       </Link> */}
+
     </div>
   );
 }
